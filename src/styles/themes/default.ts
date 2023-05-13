3a7f541df70de394929aeeca67b8b7ce98ee3cab
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-13 10:11:17 -0300

    feat: create variable to store colors project

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+import { functions } from "../functions";
+
+export const defaultTheme = {
+  colors: {
+    'base-background': '#071422',
+    'base-border': '#1C2F41',
+    'base-input': '#040F1A',
+    'base-label': '#3A536B',
+    'base-profile': '#0B1B2B',
+    'base-post': '#112131', 
+    'base-span': '#7B96B2',
+    'base-subtitle': '#C4D4E3',
+    'base-text': '#AFC2D4',
+    'base-title': '#E7EDF4',
+    blue: '#3294F8',
+  },
+  fonts: {
+    nunito: 'Nunito, sans-serif'
+  },
+  functions: functions
+}
