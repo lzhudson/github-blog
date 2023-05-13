8469478c4ea78abd5598c450a00ef6eef5499537
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-13 10:11:01 -0300

    feat: usage ThemeProvider and GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/GlobalStyles"
 
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>App</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
