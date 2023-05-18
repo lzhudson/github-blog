01bc50201d3b6cb96de1c7d62517ff94655704f0
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-18 18:09:39 -0300

    feat: add Outlet component

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -1,14 +1,19 @@
 import { HeaderContainer } from "./styles"
 import logo from '../../assets/logo.svg'
+import { Outlet } from "react-router-dom"
 
 
 export function DefaultLayout() {
   return (
-    <HeaderContainer>
-      <div className="logo-container">
-        <img src={logo} alt="Logo" />
-        <h1>Github Blog</h1>
-      </div>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <div className="logo-container">
+          <img src={logo} alt="Logo" />
+          <h1>Github Blog</h1>
+        </div>
+      </HeaderContainer>
+      <Outlet />
+    </>
+
   )
 }
