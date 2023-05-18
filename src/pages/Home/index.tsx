c16b326d44aeee2cce44df22497fc1a32d3127fb
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-18 18:10:35 -0300

    feat: import and usage Container, PublicationsHeader, SearchForm and PublicationsList component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,16 @@
+import { Container } from "../../components/Container";
+import { CardUser } from "./components/CardUser";
+import { PublicationsHeader } from "./components/PublicationsHeader";
+import { PublicationsList } from "./components/PublicationsList";
+import { SearchForm } from "./components/SearchForm";
+
 export function Home() {
   return (
-    <h1>Home</h1>
+    <Container>
+      <CardUser />
+      <PublicationsHeader />
+      <SearchForm />
+      <PublicationsList />
+    </Container>
   )
 }
