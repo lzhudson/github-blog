9b44dacae306c5238013a172915bb6ac291a9346
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-25 19:03:35 -0300

    feat: get user data from github and pass data to components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
 import { Container } from "../../components/Container";
 import { CardUser } from "./components/CardUser";
 import { PublicationsHeader } from "./components/PublicationsHeader";
 import { PublicationsList } from "./components/PublicationsList";
 import { SearchForm } from "./components/SearchForm";
+import { api } from "../../api/axios";
+
+export interface User {
+  avatar_url: string;
+  bio: string;
+  company: string;
+  html_url: string;
+  followers: string;
+  login: string;
+  name: string;
+}
 
 export function Home() {
+  const [user, setUser] = useState<User>({} as User);
+
+  async function getGithubUser() {
+    const response = await api.get(`/users/lzhudson`)
+    const user = response.data;
+    setUser(user)
+  }
+
+  useEffect(() => {
+    getGithubUser();
+  }, [])
+
   return (
     <Container>
-      <CardUser />
+      <CardUser 
+        avatar_url={user.avatar_url}
+        bio={user.bio}
+        company={user.company}
+        followers={user.followers}
+        html_url={user.html_url}
+        login={user.login}
+        name={user.name}
+      />
       <PublicationsHeader />
       <SearchForm />
       <PublicationsList />
