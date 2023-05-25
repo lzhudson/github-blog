cf925712620319fad31b12dc0c6a6b73636cbf37
Hudson Holanda <hudsonholanda55@gmail.com>
2023-05-25 18:49:17 -0300

    feat(refact): remove margin

--- src/components/Card.ts
+++ src/components/Card.ts
@@ -6,6 +6,5 @@ export const Card = styled.div`
   background-color: ${props => props.theme.colors["base-profile"]};
   box-shadow: 0 2px 28px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
-  margin-top: -85px;
   padding: 32px 40px;
 `
